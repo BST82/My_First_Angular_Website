eb93f4e30b4b03bf7288c2222eb280be290f36bc
Bhagyashri Tamrakar <86471670+BST82@users.noreply.github.com>
2023-02-26 06:39:43 +0530

    Edubridge Learning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AsideComponent } from './aside/aside.component';
+import { MainsectionComponent } from './mainsection/mainsection.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsComponent } from './forms/forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    MainsectionComponent,
+    FooterComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/mainsection/mainsection.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainsection',
+  templateUrl: './mainsection.component.html',
+  styleUrls: ['./mainsection.component.css']
+})
+export class MainsectionComponent {
+
+}
